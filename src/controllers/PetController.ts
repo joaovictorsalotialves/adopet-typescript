7e3903c9078cbfd4b0328e8be86d8e6165d65f22
João Victor <joaovictorsalotialves12@gmail.com>
2026-04-21 16:50:31 -0300

    Criando o TipoPet

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from 'express'
+import type TypePet from '../types/TypePet'
 
-const listPets = []
+const listPets: TypePet[] = []
 
 export default class PetController {
   createPet(req: Request, res: Response) {
-    const newPet = req.body
+    const newPet = req.body as TypePet
     listPets.push(newPet)
     return res.status(201).json(newPet)
   }
---
+++ src/types/TypePet.ts
@@ -0,0 +1,9 @@
+type TypePet = {
+  id: number
+  name: string
+  species: string
+  adoption: boolean
+  age: number
+}
+
+export default TypePet
