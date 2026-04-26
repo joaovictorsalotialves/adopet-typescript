cb9e614ea45b8347559c8fbd8807821ee2cdd6d4
João Victor <joaovictorsalotialves12@gmail.com>
2026-04-26 11:59:01 -0300

    Aplicando o TipoPet no CRUD

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -9,4 +9,40 @@ export default class PetController {
     listPets.push(newPet)
     return res.status(201).json(newPet)
   }
+
+  listPets(_req: Request, res: Response) {
+    return res.status(200).json(listPets)
+  }
+
+  updatePet(req: Request, res: Response) {
+    const { id } = req.params
+    const { adoption, species, age, name } = req.body as TypePet
+
+    const pet = listPets.find(pet => pet.id === Number(id))
+
+    if (!pet) {
+      return res.status(404).json({ message: 'Pet not found' })
+    }
+
+    pet.name = name
+    pet.age = age
+    pet.species = species
+    pet.adoption = adoption
+
+    return res.status(200).json(pet)
+  }
+
+  deletePet(req: Request, res: Response) {
+    const { id } = req.params
+
+    const pet = listPets.find(pet => pet.id === Number(id))
+
+    if (!pet) {
+      return res.status(404).json({ message: 'Pet not found' })
+    }
+
+    const index = listPets.indexOf(pet)
+    listPets.splice(index, 1)
+    return res.status(200).json({ message: 'Pet deleted successfully' })
+  }
 }
