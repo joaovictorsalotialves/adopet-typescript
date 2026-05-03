d0c947358d886c0e09d3496e1d45ddd481168fd5
João Victor <joaovictorsalotialves12@gmail.com>
2026-05-03 12:22:46 -0300

    Criando um banco de dados com TypeORM

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express'
 import router from './routes'
+import 'reflect-metadata'
+import { AppDataSource } from './config/dataSource'
 
 const app = express()
 app.use(express.json())
 router(app)
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!')
+  })
+  .catch(err => {
+    console.error('Error during Data Source initialization:', err)
+  })
 
 export default app
